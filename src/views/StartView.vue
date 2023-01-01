b73809261240fd57eba4c3a394cf6f01085f9930
elisehammarstrom <elise.hammarstrom@gmail.com>
2023-01-01 18:52:32 +0100

    Animation, gradient och button, satt på StartView. Skapat button-design i css.js som kan användas överallt samt animationerna

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -2,51 +2,52 @@
 
 <body>
   <div class="background">
+    <div class="wrapper">
+      <div class="headerTextWhiteBox">
+        <h1 >QuizTime!</h1>
 
-    <div id="headertext">
-      <h1 >QuizTime!</h1>
+        <div>
+          <div>
+            <router-link
+              v-bind:to="'/create/'+lang"
+              custom
+              v-slot="{ navigate }">
+
+              <button class="custom-btn"
+                @click="navigate"
+                role="link">
+
+                {{uiLabels.createPoll}}
+              </button>
+            </router-link>
+          </div>
+
+          <br />
+
+          <div>
+            <router-link
+              v-bind:to="'/connect/'+lang"
+              custom
+              v-slot="{ navigate }">
+
+              <button class="custom-btn"
+                @click="navigate"
+                role="link">
+
+                {{uiLabels.connectPoll}}
+
+              </button>
+            </router-link>
+          </div>
+
+          <!-- byta spåråk funktion-->
+          <br />
+          <button class="custom-btn" style="position:absolute; top:0; right:0; margin:5px;"  v-on:click="switchLanguage">
+            {{uiLabels.changeLanguage}}
+          </button>
+      </div>
+
     </div>
-    
-    <div>
-
-      <div>
-        <router-link
-          v-bind:to="'/create/'+lang"
-          custom
-          v-slot="{ navigate }">
-
-          <button  
-            @click="navigate"
-            role="link">
-
-            {{uiLabels.createPoll}}
-          </button>
-        </router-link>
-      </div>
-
-      <br />
-
-      <div>
-        <router-link
-          v-bind:to="'/connect/'+lang"
-          custom
-          v-slot="{ navigate }">
-
-          <button  
-            @click="navigate"
-            role="link">
-
-            {{uiLabels.connectPoll}}
-
-          </button>
-        </router-link>
-      </div>
-
-      <!-- byta spåråk funktion-->
-      <br />
-      <button 
-      style="font-size:15px; position:absolute; top:0; right:0; margin:5px;"  v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
-
     </div>
   </div>
 </body>
@@ -103,24 +104,43 @@ export default {
 
 <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&display=swap');
+  @import '@/assets/css/style.css';
 
-  body{
-    background: rgb(254,81,154);
-    background: linear-gradient(288deg, rgba(254,81,154,1) 0%, rgba(253,187,45,1) 100%);
+  body {
+    background: linear-gradient(-45deg, white, rgb(254,81,154), rgba(253,187,45,1), red);
+    background-size: 400% 400%;
     font-family: 1em Inter 400 black;
     /* För https://www.freecodecamp.org/news/html-page-width-height/*/
     min-height: 100vh;
-    
+    animation:  animate 7s ease infinite;
   }
+
+  /*body{
+    background: rgb(254,81,154);
+    background: linear-gradient(288deg, rgba(254,81,154,1) 0%, rgba(253,187,45,1) 100%);
+    font-family: 1em Inter 400 black;
+    /* För https://www.freecodecamp.org/news/html-page-width-height/
+    min-height: 100vh;
+    
+  }*/
 
   /*Har inte scoped nu så blir konstigt på alla andra sidor behöver fixa!!! */
 
-  #headertext{
-    font-size:80px;
-    font-size: 4vw
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    text-align: center;
+    height: 100vh;
+
 
   }
 
+  .headerTextWhiteBox {
+    height: 40vh;
+  }
+/*
   button {
   border-radius: 1em;
   margin: 0px 10px 0px 10px;
@@ -130,7 +150,7 @@ export default {
   font-size:20px;
 
   background: #D7D7D7;
-}  
+}  */
 h1 { margin: 0 0 10px; }
 
 /*@media screen and (max-width:50em) {
